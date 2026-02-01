babf6e416558d64f751b5212a20130a43aeefca3
tksmasaki <tksmasaki09@gmail.com>
2026-02-01 18:46:27 +0900

    Fix carapace installation method

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p ~/.local/bin ~/.zsh
 # https://github.com/zsh-users/zsh-autosuggestions
+mkdir -p ~/.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 # https://pixi.carapace.sh
-curl -sSL https://github.com/carapace-sh/carapace-bin/releases/latest/download/carapace-bin_linux_amd64.tar.gz | tar -xz -C ~/.local/bin
+mise use -g carapace@latest
 
 ln -fs ~/dotfiles/.zshrc ~/.zshrc
 source ~/.zshrc
