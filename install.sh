d19b60b718a49fc1b2db97756687a93900041896
tksmasaki <tksmasaki09@gmail.com>
2026-02-01 19:27:17 +0900

    Revert "Test .bashrc config"
    This reverts commit fed52b9b1c5ba7281aa110e41f2f55dbc24843dd.

--- install.sh
+++ install.sh
@@ -9,9 +9,4 @@ mise use -g carapace@latest
 echo "" >> ~/.zshrc
 echo "# Load dotfiles configuration" >> ~/.zshrc
 cat ~/dotfiles/.zshrc >> ~/.zshrc
-
-echo "" >> ~/.bashrc
-echo "# Load dotfiles configuration" >> ~/.bashrc
-echo "alias lla='ls -lAF --color=auto'" >> ~/.bashrc
-
 source ~/.zshrc
